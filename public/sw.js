d5202628c294308ac971e8a9c875474ce2533976
Linda <lindavandijkdesign@gmail.com>
2016-03-16 12:27:21 +0100

    service worker

---
+++ public/sw.js
@@ -0,0 +1,25 @@
+this.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open('looklive-v1').then(cache => {
+      return cache.addAll([
+        './',
+        '/styles/style.css',
+        '/js/app.js',
+        '/images/logo.png',
+        'images/header.jpg'
+      ]);
+    })
+  );
+});
+
+
+this.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        cache.put(event.request, response.clone());
+        return response || fetch(event.request)
+      })
+  );
+});
+
